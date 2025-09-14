814f7a0b8db4e8b4f2b421ebc2828cd3ac7b9112
goraedoll <goraedoll21@gmail.com>
2025-09-14 21:58:43 +0900

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by 고래돌 on 9/14/25.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
